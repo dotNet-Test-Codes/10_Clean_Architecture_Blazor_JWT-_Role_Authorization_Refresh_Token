8b26eab669ca9a642d168be769e22667d78ba58a
Rashmika Madushan Samarakoon <rashmika@codezync.com>
2024-07-08 21:39:35 +0530

    Add AccountRepository

---
+++ Solusion/Infrastructure/Repos/AccountRepository.cs
@@ -0,0 +1,134 @@
+﻿using Application.Contracts;
+using Application.DTOs.Request.Account;
+using Application.DTOs.Response;
+using Application.DTOs.Response.Account;
+using Application.Extensions;
+using Domain.Entity.Authentication;
+using Mapster;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Infrastructure.Repos
+{
+    public class AccountRepository(RoleManager<IdentityRole> roleManager,
+                                   UserManager<ApplicationUser> userManager,
+                                   SignInManager<ApplicationUser> signInManager,
+                                   IConfiguration config) : IAccount
+    {
+        public Task<GeneralResponse> ChangeUserRoleAsync(ChangeUserRoleRequestDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralResponse> CreateAccountAsync(CreateAccountDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task CreateAdmin()
+        {
+            try
+            {
+                if ((await FindRoleByNameAsync(Constant.Role.Admin)) != null) return;
+
+                var admin = new CreateAccountDTO()
+                {
+                    Name = "Admin",
+                    Password = "Admin@123",
+                    EmailAddress = "admin@gmail.com",
+                    Role = Constant.Role.Admin
+                };
+
+                await CreateAccountAsync(admin);
+            }
+            catch { }
+        }
+
+        public Task<GeneralResponse> CreateRoleAsync(CreateRoleDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GetRoleDTO>> GetRoleAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GetUsersWithRolesResponseDTO>> GetUsersWithRolesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponse> LoginAccountAsync(LoginDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Private
+        private async Task<ApplicationUser> FindUserByEmailAsync(string email) =>
+            await userManager.FindByEmailAsync(email);
+
+        private async Task<IdentityRole> FindRoleByNameAsync(string roleName) =>
+            await roleManager.FindByNameAsync(roleName);
+
+        private static string GenerateRefreshToken() =>
+            Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
+
+        private async Task<string> GenerateToken(ApplicationUser user)
+        {
+            try
+            {
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]!));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+                var userClaims = new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Email),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Role, (await userManager.GetRolesAsync(user)).FirstOrDefault().ToString()),
+                    new Claim("FullName", user.Name)
+                };
+
+                var token = new JwtSecurityToken(issuer: config["Jwt:Issuer"],
+                                                 audience: config["Jwt:Audience"],
+                                                 claims: userClaims,
+                                                 expires: DateTime.Now.AddMinutes(30),
+                                                 signingCredentials: credentials);
+
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+            catch { return null!; }
+        }
+
+        private async Task<GeneralResponse> AssignUserToRole(ApplicationUser user, IdentityRole role)
+        {
+            if (user is null || role is null) return new GeneralResponse(false, "Model state cannot be empty!");
+            if (await FindRoleByNameAsync(role.Name) == null)
+                await CreateRoleAsync(role.Adapt(new CreateRoleDTO()));
+
+            IdentityResult result = await userManager.AddToRoleAsync(user, role.Name);
+            string error = CheckResponse(result);
+
+            if (!string.IsNullOrEmpty(error))
+                return new GeneralResponse(false, error);
+            else
+                return new GeneralResponse(true, $"{user.Name} assigned to {role.Name} role");
+        }
+
+        public static string CheckResponse(IdentityResult result)
+        {
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(x => x.Description);
+                return string.Join(Environment.NewLine, errors);
+            }
+
+            return null!;
+        }
+        #endregion
+    }
+}
