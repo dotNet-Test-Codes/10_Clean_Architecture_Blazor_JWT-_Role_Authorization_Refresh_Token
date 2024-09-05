f19483e17fca17d98a28056526b27dde06a06379
Rashmika Madushan Samarakoon <rashmika@codezync.com>
2024-09-05 22:49:31 +0530

    AuthenticationLayout and nav menue

--- Solusion/WebUI/Program.cs
+++ Solusion/WebUI/Program.cs
@@ -1,11 +1,12 @@
+using Application.DependencyInjection;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using NetcodeHub.Packages.Components.Toast;
 using WebUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddApplicationService();
+builder.Services.AddScoped<ToastService>();
 await builder.Build().RunAsync();
